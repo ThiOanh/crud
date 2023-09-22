23cc9e91f4d6d955842b7aa61c2349b221019827
ThiOanh <nguyenthioanh170821@gmail.com>
2023-09-22 13:01:40 +0700

    change host BE

--- src/libraries/axiosClient.js
+++ src/libraries/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: 'https://be-vqnz.onrender.com',
   headers: { "Content-Type": "application/json" },
 
 });
